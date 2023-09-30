f87c6245c02cb07efd0186e0b6fe95e38d09fe6b
Uditp11 <88726989+Uditp11@users.noreply.github.com>
2023-09-30 21:31:13 +0530

    Nav3

--- T1_LGM.js
+++ T1_LGM.js
@@ -1,4 +1,4 @@
-// Sticky Navigation Menu JS Code
+// The Sticky Navigation Menu JS Code
 let nav = document.querySelector("nav");
 let scrollBtn = document.querySelector(".scroll-button a");
 console.log(scrollBtn);
